1056ca11046c51c4d38f1e215c0ed83d97f63145
Chris Martin <chris.martin@metova.com>
2017-12-25 15:14:02 -0500

    Add file output

---
+++ Sources/ObjectCreator/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  ObjectCreatorPackageDescription
+//
+//  Created by Chris Martin on 12/25/17.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let defaultOutputDirectory = "export"
+}
